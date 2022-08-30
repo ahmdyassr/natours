5fed8e4cc5ce9bbab4445281dc895c03fe9f083c
Ahmed Yasser <ahmdyassr@gmail.com>
2022-08-31 00:13:11 +0200

    More progress into the NodeJS Course

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -12,7 +12,7 @@ const getAllTours = async (req, res) => {
 			}
 		})
 	} catch (e) {
-		res.status(500).json({
+		res.status(404).json({
 			status: 'fail',
 			message: e 
 		})
@@ -30,7 +30,7 @@ const createTour = async (req, res) => {
 			}
 		})
 	} catch(e) {
-		res.status(400).json({
+		res.status(404).json({
 			status: 'fail',
 			message: e
 		})
@@ -48,7 +48,7 @@ const getTour = async (req, res) => {
 			}
 		})
 	} catch (e) {
-		res.status(500).json({
+		res.status(404).json({
 			status: 'fail',
 			message: e
 		})
@@ -57,7 +57,10 @@ const getTour = async (req, res) => {
 
 const updateTour = async (req, res) => {
 	try {
-		const tour = await Tour.findByIdAndUpdate(req.params.id, req.body)
+		const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true
+		})
 
 		res.status(201).json({
 			status: 'success',
@@ -66,7 +69,7 @@ const updateTour = async (req, res) => {
 			}
 		})
 	} catch(e) {
-		res.status(500).json({
+		res.status(404).json({
 			status: 'fail',
 			message: e
 		})
@@ -75,16 +78,14 @@ const updateTour = async (req, res) => {
 
 const deleteTour = async (req, res) => {
 	try {
-		const tour = await Tour.findByIdAndDelete(req.params.id, req.body)
+		await Tour.findByIdAndDelete(req.params.id, req.body)
 
-		res.status(200).json({
+		res.status(204).json({
 			status: 'success',
-			data: {
-				tour
-			}
+			data: null
 		})
 	} catch(e) {
-		res.status(500).json({
+		res.status(404).json({
 			status: 'fail',
 			message: e
 		})
--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -4,16 +4,49 @@ const tourSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
-		unique: true
+		unique: true,
+		trim: true
 	},
-	rating: {
+	duration: {
+		type: Number,
+		required: true
+	},
+	maxGroupSize: {
+		type: Number,
+		required: true
+	},
+	difficulty: {
+		type: String,
+		required: true
+	},
+	ratingsAverage: {
 		type: String,
 		default: 4.5
+	},
+	ratingsQuantity: {
+		type: Number,
+		default: 0
 	},
 	price: {
 		type: Number,
 		required: true
-	}
+	},
+	priceDicsount: {
+		type: Number
+	},
+	summary: {
+		type: String,
+		trim: true // remove white space at the begining and the end of the a string
+	},
+	imageCover: {
+		type: String
+	},
+	images: [String],
+	createdAt: {
+		type: Date,
+		default: Date.now()
+	},
+	startDates: [Date]
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
